5549177d8d8b1b5d8e5447e1e205285bc382e444
Arun Galva <arungalva@gmail.com>
2015-12-07 23:15:43 -0500

    Added css files

--- SYSC2001-Aider/converter.js
+++ SYSC2001-Aider/converter.js
@@ -1,47 +1,85 @@
-function binary(num){
-			    this.value = num;
-
-			    this.toBaseFour = function()
-			    {
-			    	console.log(parseInt(this.value,2).toString(4));
-			    };
-
-			    this.toOct = function()
-			    {
-			    	console.log(parseInt(this.value,2).toString(8))
-			    };
-
-			    this.toDecimal = function()
-			    {
-			    	console.log(parseInt(this.value,2).toString(10));			    	
-			    };
-
-			    this.toHex = function()
-			    {
-			    	console.log(parseInt(this.value,2).toString(16));	
-			    };
-			};
-
-			function binaryToDeci(binaryNumber)
-			{
-				var binaryNum = new binary(binaryNumber);
-				binaryNum.toDecimal();
-			}
-
-			function binaryToHex(binaryNumber)
-			{
-				var binaryNum = new binary(binaryNumber);
-				binaryNum.toHex();
-			}
-
-			function binaryToOct(binaryNumber)
-			{
-				var binaryNum = new binary(binaryNumber);
-				binaryNum.toOct();
-			}
-
-			function binaryToBaseFour(binaryNumber)
-			{
-				var binaryNum = new binary(binaryNumber);
-				binaryNum.toBaseFour();
-			}
+function number(){
+	this.bin2B4 = function(num)
+	{
+	    return parseInt(num.toString(),2).toString(4);
+	};
+	
+	this.bin2Oct = function(num)
+	{
+	    return parseInt(num.toString(),2).toString(8);
+	};
+	
+	this.bin2Deci = function(num)
+	{
+	    return parseInt(num.toString(),2).toString(10);			    	
+	};
+	
+	this.bin2Hex = function(num)
+	{
+		return parseInt(num.toString(),2).toString(16);
+	}
+	
+	this.b42Bin = function(num)
+	{
+	    return parseInt(num.toString(),4).toString(2);
+	};
+	
+	this.b42Oct = function(num)
+	{
+	    return parseInt(num.toString(),4).toString(8);
+	};
+	
+	this.b42Hex = function(num)
+	{
+	    return parseInt(num.toString(),4).toString(16);
+	};
+	
+	this.b42Dexi = function(num)
+	{
+	    return parseInt(num.toString(),4).toString(10);
+	};
+	
+	this.oct2Deci = function(num)
+	{
+	    return parseInt(num.toString(),8).toString(10);
+	};
+	
+	this.oct2B4 = function(num)
+	{
+	    return parseInt(num.toString(),8).toString(4);
+	};
+	
+	this.oct2Hex = function(num)
+	{
+	    return parseInt(num.toString(),8).toString(16);
+	};
+	
+	this.oct2Bin = function(num)
+	{
+	    return parseInt(num.toString(),8).toString(2);
+	};
+	
+	this.hex2Bin = function(num)
+	{
+	    return parseInt(num.toString(),16).toString(2);
+	};
+	
+	this.hex2B4 = function(num)
+	{
+	    return parseInt(num.toString(),16).toString(4);
+	};
+	
+	this.hex2Oct = function(num)
+	{
+	    return parseInt(num.toString(),16).toString(8);
+	};
+	
+	this.hex2Deci = function(num)
+	{
+	    return parseInt(num.toString(),16).toString(10);
+	};
+	
+	
+};
+	
+var num = new number();
